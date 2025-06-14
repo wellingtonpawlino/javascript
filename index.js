a5ad7469d42bf365a10fff5cd5827d5cb34ef358
Wellington Pawlino <wellingtonpawlino@gmail.com>
2025-06-14 18:01:38 -0300

    atualiza index.js

--- index.js
+++ index.js
@@ -29,3 +29,76 @@ function calcularPrecoComDesconto(produto) {
     return produto.preco * (1 - produto.desconto);
 }
 
+// operdores logicos
+
+let corCliente = undefined;
+let corEstoque = "vermelho";
+let corVendido = corCliente || corEstoque
+
+console.log(corVendido + " é a cor do produto vendido!");
+
+
+// criando estrutura else if 
+
+let driverSpeed = 100;
+
+if (driverSpeed > 100) {  
+    console.log("Você está acima do limite de velocidade!");
+}
+else if (driverSpeed < 60) {
+    console.log("Você está abaixo do limite de velocidade!");
+}
+else {  
+    console.log("Você está dentro do limite de velocidade!");
+}
+
+
+// criand with switch case
+let airport = 'LAX';
+
+switch (airport) {
+    case 'MCO':
+        console.log('Orlando International Airport');
+        break;
+    case 'LAX':
+        console.log('Los Angeles International Airport');
+        break;
+    case 'JFK':
+        console.log('John F. Kennedy International Airport');
+        break;
+    default:
+        console.log('Airport not recognized');
+}
+
+// criando loop for
+for (let i = 0; i < 11; i++) {
+    console.log("Número: " + i);
+}
+
+// criando while loop
+let contador = 0;
+while (contador < 5) {
+    console.log("Contador: " + contador);
+    contador++;
+}
+
+// criando do while loop   
+do {
+    console.log("Contador: " + contador);
+    contador++;
+}while (contador < 11);
+
+
+const myCar = {
+    model: 'Corolla',
+    year: 2020,
+    color: 'blue'
+}
+
+for (let key in myCar) {
+    console.log(`${key}: ${myCar[key]}`);
+}
+
+for (let value of Object.values(myCar)) {
+    console.log(value);
+}
